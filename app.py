e2ad5de7a8720f4c626f006415cb41e2d8fb5d3e
TEJAS MARAMWAR <tejasmaramwar01@gmail.com>
2023-06-01 16:38:43 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, url_for, flash, redirect, request, send_from_directory, Response
+import os
+from os.path import join, dirname, realpath
+from werkzeug.utils import secure_filename
+from os.path import join, dirname, realpath
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template, flash
+from werkzeug.utils import secure_filename
+import pipeline
+
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads/')
+# ALLOWED_EXTENSIONS = {'png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG', 'gif', 'GIF'}
+
+config = {
+    "DEBUG": True
+}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+app.secret_key = 'key'
+
+@app.route('/')
+def home():
+    return render_template('index.html', result=None)
+
+@app.route('/assessment', methods=['GET', 'POST'])
+def upload_and_classify():
+    if request.method == 'POST':
+
+        file = request.files['file']
+
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        model_results = pipeline.pipe(filepath)
+        print(type(model_results))
+        return render_template('results.html', result=model_results, scroll='third', filename=filename)
+
+    flash('Invalid file format. Please try again.')
+    return redirect(url_for('assess'))
+
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False,)
---
+++ static/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // Adding smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#top'], .container-fluid a[href='#third']").on('click', function (event) {
+
+      // Making sure this hash has a value before overriding default behavior
+      if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function () {
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if 
+    });
+  })
+
+
+function loading() {
+    $("#loading").show();
+}
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
